531166847b388070211d798b34bf2b8d9040f5f6
qgwright <qgwright@hotmail.com>
2019-02-24 03:51:50 -0500

    cleanup

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,163 +1,170 @@
+var panel = $('#quiz-area');
+var countStartNumer = 30;
+var Smurfs_Theme_Song = new Audio("./assets/sound/Smurfs_Theme_Song.mp3");
+var gargamel = new Audio("./assets/sound/gargamel.mp3")
 
-$(document).ready(function(){
+
+$(document).ready(function() {
+   Smurfs_Theme_Song.play();
+});
 
     // start the game when user clicks on Start button
-    $("#start-button").on("click", gameStatus.startTimer);
-  
+    $(document).on('click', '#start-over', function(e) {
+      game.reset();
+  });
+
+  $(document).on('click', '.answer-button', function(e) {
+      game.clicked(e);
   });
   
-  // information about the state of game play
-  var gameStatus = {
+  $(document).on('click', '#start', function(e) {
+      $('.timer').append('<h2>Time Remaining: <span id="counter-number">30</span> Seconds</h2>');
+      game.loadQuestion();
+      Smurfs_Theme_Song.pause();
+  });
   
-    // set the time at 60 seconds, and count down by 1 second
-    timeRemaining : 60,
-  
-    // start the timer, hide the start page, show the questions
-    startTimer: function() {
-      $("#timer").text("Time remaining: " + gameStatus.timeRemaining);
-      setInterval(gameStatus.countdown, 1000);
-      $("#start-page").hide();
-      trivia.displayQuestions();
-    },
-  
-    // decrement the timer and update the UI; stop the timer at 0
-    countdown: function() {
-      gameStatus.timeRemaining--;
-      $("#timer").text("Time remaining: " + gameStatus.timeRemaining);
-      if (gameStatus.timeRemaining === 0) {
-        gameStatus.stopTimer();
-        $("#timer").empty();
-      }
-    },
-  
-    // stop the timer and check the answers
-    stopTimer: function() {
-      clearInterval();
-      trivia.checkAnswers();
-    },
-  
-    // hide the quetions and display the end page with results
-    showEndPage: function(numCorrect, numIncorrect, numUnanswered) {
-      $("#end-page").show();
-      $("#questions-box").empty();
-      $("#timer").empty();
-      $("#timer").hide();
-      $("#correct-answers").text("Correct answers (smurferrific!): " + numCorrect);
-      $("#incorrect-answers").text("Incorrect answers (aww smurf!): " + numIncorrect);
-      $("#unanswered").text("Skipped questions (): " + numUnanswered);
-    }
-  }
-  
-  // functions to handle the building questions page and scoring
-  var trivia = {
-  
-    // pull questions from the array of questions, loop through them, and append to UI
-    displayQuestions: function() {
-      var divContainer = $("#questions-box");
-      var answerGroup = $(".form-check");
-      divContainer.append('<h2>Answer the following questions:</h2>');
-              
-      for (var i = 0; i < questionBank.length; i++) {
-  
-        divContainer.append('<div id="question">' + questionBank[i].question + '</div>');
-  
-        var answer1 = questionBank[i].answers[0];
-        var answer2 = questionBank[i].answers[1];
-        var answer3 = questionBank[i].answers[2];
-  
-        divContainer.append('<div class="form-check"><input class="form-check-input" type="radio" name="radio-group'+i+'" id="radio'+i+'"><label class="form-check-label" id="radio'+i+'label" for="radio'+i+'">' + answer1 + '</label></div>');
-        divContainer.append('<div class="form-check"><input class="form-check-input" type="radio" name="radio-group'+i+'" id="radio'+i+'"><label class="form-check-label" id="radio'+i+'label" for="radio'+i+'">' + answer2 + '</label></div>');
-        divContainer.append('<div class="form-check"><input class="form-check-input" type="radio" name="radio-group'+i+'" id="radio'+i+'"><label class="form-check-label" id="radio'+i+'label" for="radio'+i+'">' + answer3 + '</label></div>');
-      }
-  
-      // add a Done button to the end of the page and register its click handler
-      var doneButton = '<button class="btn btn-primary" id="done-button" type="submit">Done</button>';
-      divContainer.append(doneButton);
-      $("#done-button").on("click", gameState.stopTimer);
-    },
-  
-    // test if the user answers are correct, incorrect, or if there are unanswered questions
-    checkAnswers: function() {
-      var correctAnswer;
-      var userAnswer;
-      var numCorrect = 0;
-      var numIncorrect = 0;
-      var numUnanswered = 0;
-  
-      // loop through to compare the text of the label with the user answers
-      // increment score counts appropriately
-      for (var i = 0; i < questionBank.length; i++) {
-        correctAnswer = questionBank[i].correct;
-        userAnswer = $('input[id=radio'+i+']:checked + label').text();
-  
-        if (userAnswer === correctAnswer) {
-          numCorrect++;
-        } else if (userAnswer === "") {
-          numUnanswered++;
-        } else if (userAnswer !== correctAnswer) {
-          {
-            numIncorrect++;
-          }
-        }
-      }
-  
-      // show the end page with the score tally
-      gameStatus.showEndPage(numCorrect, numIncorrect, numUnanswered);
-    },
-  }
-  
+  ///////////////////////////////////////////////////////////////////////////////
+  //Question set
+  //////////////////////////////////////////////////////////////////
   // array of objects with the questions, possible answers, and the correct answer
-  var questionBank =
-  [
-    {
+  var questions =[{
       question: "Which smurf wore glasses?",
       answers: ["Vanity Smurf,Brainy Smurf, Bifocal Smurf"],
       correct: "Brainy Smurf"
-    },
-  
-    {
+    },{
       question: "What year did the smurfs animated television series air in the U.S?",
       answers: ["1979, 1980, 1981"],
       correct: "1981"
-    },
-    {
+    }, {
       question: "What do the Smurfs live in?",
       answers: ["molehills", "caves", "mushrooms"],
       correct: "mushrooms"
-    },
-    {
+    }, {
       question: "Which character is in charge of the Smurfs?",
       answers: ["Brainy Smurf", "Commander Smurf", "Papa Smurf"],
       correct: "Papa Smurf"
-    },
-    {
+    },{
       question: "What color is Smurfettes hair?",
       answers: ["red", "white", "blond"],
       correct: "blond"
-    },
-    {
+    },{
       question: "What was the name of the villan in The Smurfs?",
       answers: ["Azreal", "Gargamel", "Mumm ra"],
       correct: "Gargamel"
-    },
-    {
+    },{
       question: "What color are Papa Smurfs hat and pants?",
       answers: ["white", "red", "yellow"],
       correct: "red"
-    },
-    {
+    },{
       question: "What is tha name of Gargamels cat?",
       answers: ["Frisky", "Azreal", "Rumplestiltskin"],
       correct: "Azreal"
-    },
-    {
+    },{
       question: "How many female smurfs are there?",
       answers: ["one", "three", "two"],
       correct: "three - Smurfette,Sassette Smurfling,and Nanny Smurf"
-    },
-    {
+    },{
       question: "Which Smurf wears yellow clothes?",
       answers: ["Papa Smurf", "Happy Smurf","Granpa Smurf"],
       correct: "Granpa Smurf"
-    }
-    ];
+    }];
+
+
+var game = {
+  questions: questions,
+  currentQuestion: 0,
+  counter: countStartNumber,
+  correct: 0,
+  incorrect: 0,
+  countdown: function() {
+      game.counter--;
+      $('#counter-number').html(game.counter);
+
+      if (game.counter === 0) {
+          console.log('TIME UP');
+          game.timeUp();
+      }
+  },
+  loadQuestion: function() {
+      timer = setInterval(game.countdown, 1000);
+      panel.html('<h2>' + questions[this.currentQuestion].question + '</h2>');
+      for (var i = 0; i < questions[this.currentQuestion].answers.length; i++) {
+          panel.append('<button class="answer-button" id="button"' + 'data-name="' + questions[this.currentQuestion].answers[i] + '">' + questions[this.currentQuestion].answers[i] + '</button>');
+      }
+  },
+  nextQuestion: function() {
+      game.counter = countStartNumber;
+      $('#counter-number').html(game.counter);
+      game.currentQuestion++;
+      game.loadQuestion();
+  },
+  timeUp: function() {
+      badSound.play();
+      clearInterval(timer);
+      $('#counter-number').html(game.counter);
+
+      panel.html('<h2>Out of Time!</h2>');
+      panel.append('<h3>The Correct Answer was: ' + questions[this.currentQuestion].correctAnswer);
+      panel.append('<img src="' + questions[this.currentQuestion].image + '" />');
+
+      if (game.currentQuestion === questions.length - 1) {
+          setTimeout(game.results, 3 * 1000);
+      } else {
+          setTimeout(game.nextQuestion, 3 * 1000);
+      }
+  },
+  results: function() {
+      finalSound.play();
+      clearInterval(timer);
+
+      panel.html('<h2>All done, heres how you did!</h2>');
+      $('#counter-number').html(game.counter);
+      panel.append('<h3>Correct Answers: ' + game.correct + '</h3>');
+      panel.append('<h3>Incorrect Answers: ' + game.incorrect + '</h3>');
+      panel.append('<h3>Unanswered: ' + (questions.length - (game.incorrect + game.correct)) + '</h3>');
+      panel.append('<br><button id="start-over">Start Over?</button>');
+  },
+  clicked: function(e) {
+      clearInterval(timer);
+
+      if ($(e.target).data("name") === questions[this.currentQuestion].correctAnswer) {
+          this.answeredCorrectly();
+      } else {
+          this.answeredIncorrectly();
+      }
+  },
+  answeredIncorrectly: function() {
+      badSound.play();
+      game.incorrect++;
+      clearInterval(timer);
+      panel.html('<h2>Nope!</h2>');
+      panel.append('<h3>The Correct Answer was: ' + questions[game.currentQuestion].correctAnswer + '</h3>');
+      panel.append('<img src="' + questions[game.currentQuestion].image + '" />');
+
+      if (game.currentQuestion === questions.length - 1) {
+          setTimeout(game.results, 3 * 1000);
+      } else {
+          setTimeout(game.nextQuestion, 3 * 1000);
+      }
+  },
+  answeredCorrectly: function() {
+      gargamel.play();
+      clearInterval(timer);
+      game.correct++;
+      panel.html('<h2>Correct!</h2>');
+      panel.append('<img src="' + questions[game.currentQuestion].image + '" />');
+
+      if (game.currentQuestion === questions.length - 1) {
+          setTimeout(game.results, 3 * 1000);
+      } else {
+          setTimeout(game.nextQuestion, 3 * 1000);
+      }
+  },
+  reset: function() {
+      this.currentQuestion = 0;
+      this.counter = countStartNumber;
+      this.correct = 0;
+      this.incorrect = 0;
+      this.loadQuestion();
+  }
+};
